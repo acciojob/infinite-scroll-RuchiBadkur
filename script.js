2fc1dca840a39d19874dcf2908fabd3905c8bb47
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-23 13:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+const main = document.querySelector("main");
+let i = 1, n = 10; 
+
+const createLi = (i) => {
+	const li = document.createElement("li");
+	li.innerText = `item ${i}`;
+	list.append(li);
+}
+
+for(i = 1; i <= n; i++){
+	createLi(i);
+}
+
+const addNewLi = () => {
+	n += 2;
+	for(i = i; i <= n; i++){
+		createLi(i);
+	}
+}
+
+list.addEventListener("scrollend", addNewLi);
